c835474b1c3a97b32e3ae6ae9e10ff3a26c76ae0
Brian Anderson <brian@hop.life>
2022-07-14 16:45:11 -0500

    change port and env variables to match what is being used in other samples

--- server/node/README.md
+++ server/node/README.md
@@ -3,13 +3,13 @@ Add your api token to the Dockerfile.
 
 ```bash
 docker build . -t safehello-server
-docker run -p 3000:3000 safehello-server
+docker run -p 80:8080 safehello-server
 ```
 
 You may then access the services from your browser:
-http://localhost:3000/tokens/sample-user-id
-http://localhost:3000/create/sample-user-id
-http://localhost:3000/get/<EVENT_ID>
+http://localhost/tokens/sample-user-id
+http://localhost/create/sample-user-id
+http://localhost/get/<EVENT_ID>
 
 For complete documentation of the SDK, see here:
 https://github.com/SafeHello/safehello-sdk-samples/wiki/6.4-Rest-API-Overview
--- server/node/index.js
+++ server/node/index.js
@@ -1,16 +1,18 @@
 const { SafeHello } = require('@safehello/sdk-typescript')
 const express = require('express')
 const app = express()
-const port = 3000
-const apiToken = process.env.API_TOKEN
+const port = 8080
+
+const apiToken = process.env.TOKEN
 const safehelloEnv = process.env.SAFEHELLO_ENV || 'prod'
+
 /**
  * With the apiToken, you can call all the methods of the SDK.
  */
 const apiClient = new SafeHello(apiToken, safehelloEnv)
 
 app.get('/tokens/:userId', async (req, res) => {
-  // ie: http://localhost:3000/tokens/sample-user-id
+  // ie: http://localhost:8080/tokens/sample-user-id
   const { userId } = req.params
   const userToken = await apiClient.getAuthToken(userId)
   /*
@@ -21,14 +23,14 @@ app.get('/tokens/:userId', async (req, res) => {
 })
 
 app.get('/create/:userId', async (req, res) => {
-  // ie: http://localhost:3000/create/sample-user-id
+  // ie: http://localhost:8080/create/sample-user-id
   const { userId } = req.params
   const event = await apiClient.createEvent({ senderId: userId })
   res.send(event)
 })
 
 app.get('/get/:eventId', async (req, res) => {
-  // ie: http://localhost:3000/get/01G7YRQQ45B8GS5JDTPZ1RK401
+  // ie: http://localhost:8080/get/01G7YRQQ45B8GS5JDTPZ1RK401
   const { eventId } = req.params
   const event = await apiClient.getEvent(eventId)
   res.send(event)
