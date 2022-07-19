501ad1d7c6ffb1aacf4f1928b2c7440ed8c4b2e3
Jose <Abreu>
2022-07-19 18:03:43 -0400

    Change the endpoint based on the environment

--- iOS/server/Sources/App/routes.swift
+++ iOS/server/Sources/App/routes.swift
@@ -1,11 +1,10 @@
 import Vapor
 
 fileprivate let token = Environment.get("TOKEN")!
-fileprivate let safeHelloTokensEndpoint: URI = "https://api.prod.safehello.com/auth/tokens"
 
 func routes(_ app: Application) throws {
     app.get("tokens", ":userId") { req async throws -> TokenResponse in
-        return try await req.client.post(safeHelloTokensEndpoint) { clientReq in
+        return try await req.client.post(app.environment.safeHelloTokensEndpoint) { clientReq in
             clientReq.headers.add(name: "x-api-key", value: token)
             try clientReq.content.encode(["userId": req.parameters.get("userId")!])
         }.content.decode(TokenResponse.self)
@@ -14,4 +13,19 @@ func routes(_ app: Application) throws {
 
 fileprivate struct TokenResponse: Content {
     let token: String
+}
+
+fileprivate extension Environment {
+    var safeHelloTokensEndpoint: URI {
+        switch self {
+        case .development:
+            return "https://api.dev.safehello.com/v1/auth/tokens"
+        case .testing:
+            return "https://api.qa.safehello.com/v1/auth/tokens"
+        case .production:
+            fallthrough
+        default:
+            return "https://api.prod.safehello.com/v1/auth/tokens"
+        }
+    }
 }
