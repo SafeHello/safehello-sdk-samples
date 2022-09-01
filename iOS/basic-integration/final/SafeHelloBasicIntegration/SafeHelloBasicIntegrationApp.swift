01c134a1a7c82b273701746ead1c6557d7a46a69
Jose <Abreu>
2022-09-01 16:43:30 -0400

    Update iOS sample to request location permission

--- iOS/basic-integration/final/SafeHelloBasicIntegration/SafeHelloBasicIntegrationApp.swift
+++ iOS/basic-integration/final/SafeHelloBasicIntegration/SafeHelloBasicIntegrationApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import CoreLocation
 
 @main
 struct SafeHelloBasicIntegrationApp: App {
+    private let location = CLLocationManager()
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onChange(of: scenePhase) { newValue in
+                    switch newValue {
+                    case .active:
+                        location.requestWhenInUseAuthorization()
+                    default:
+                        break
+                    }
+                }
         }
     }
 }
